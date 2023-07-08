8826b4c766ade062806a2d71c93272b92f4bf253
Abdullahlionn <80156629+Abdullalionn@users.noreply.github.com>
2023-07-08 23:15:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,6 @@ prompt = st.text_input('', 'Enter a prompt')
 def open_exe(exe_path):
   os.startfile(exe_path)
 
-if "sublime" in message:
-    open_exe("C:\\Program Files\\Sublime Text 3\\sublime_text.exe")
-    print("opening sublime")
-elif "vs" in message:
-    open_exe('C:\\Users\\lenovo\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe')
-    print("opening visual studios code")
-elif "pycharm" in message:
-    open_exe("C:\\Program Files\\JetBrains\\PyCharm Community Edition 2022.3.3\\bin\\pycharm64.exe")
-    print("PyCharm")
-elif "qtd" in message:
-    open_exe()
-else:
-    st.text((Bard().get_answer(str(prompt))['content']))
+st.text((Bard().get_answer(str(prompt))['content']))
 
 #print(Bard().get_answer(str(message))['content'])
